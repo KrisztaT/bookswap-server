7b55b36823b2db240818059a5c5d6de7c4aaaa85
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-07-11 14:24:19 +1000

    Join static method and controller added

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema= mongoose.Schema
 
+// create user schema object
 const userSchema = new Schema({
     username: {
 		type: String,
@@ -27,5 +29,34 @@ const userSchema = new Schema({
     }
 
 })
+
+
+//static join method
+userSchema.statics.join = async function(username, first_name, email, password){
+
+     // checking if there is already a user in the database with the same username 
+     const usernameExists = await this.findOne({username})
+   // checking if there is already a user in the database with the same email address 
+    const emailExists = await this.findOne({email})
+   
+    // throw error if username already exists in the database
+    if(usernameExists){
+        throw Error("Username is already in use")
+    }
+
+    // throw error if email already exists in the database
+    if(emailExists){
+        throw Error("Email is already in use")
+    }
+    
+    // salting and hashing the password using bcrypt
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    // Creating a new user object in the database using the Mongoose `create` method.
+    const user = await this.create({username, first_name, email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
