94cb37be3a8a6bdf061f2003eec3588d86c99d7d
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-07-11 10:28:02 +1000

    Create user model

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const Schema= mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+		type: String,
+		unique: true,
+		uniqueCaseInsensitive: true,
+		required: true,
+        minLength: 5
+	},
+    first_name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+
+})
+
+module.exports = mongoose.model('User', UserSchema)
